3f86941a48e15a15b2028bd5f7a64dc349dfa653
Moldir <51477205+Moldir99@users.noreply.github.com>
2022-04-30 20:26:30 +0300

    Adding Day14 code
    Topic is Scope

---
+++ Day14.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+
+class Difference{
+  private int[] elements;
+  public int maximumDifference;
+
+  //Constructor: 
+  Difference(int[] elements){
+    this.elements = elements;
+  }
+
+  public void computeDifference(){
+    int max = 0;
+
+    for(int i = 0; i < elements.length; i++){
+      for(int j = 0; j < elements.length; j++){
+        int abs = Math.abs(elements[i] - elements[j]);
+        if(abs > max){
+          max = abs;
+        }
+      }
+    }
+    maximumDifference = max;
+  }
+}
+
+public class Day14{
+
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+      a[i] = sc.nextInt();
+    }
+    sc.close();
+    
+    Difference difference = new Difference(a);
+    difference.computeDifference();
+    System.out.println(difference.maximumDifference);
+  }
+}
+
+/* 
+Sample input: 
+3
+1 2 5
+
+Sample output:
+4
+*/
